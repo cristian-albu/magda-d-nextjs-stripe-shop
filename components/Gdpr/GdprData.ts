33667be94f7b69f90cc7a40d5cc08629146ffbfa
cristian-albu <albucristian95p@gmail.com>
2023-09-27 15:19:57 +0300

    privacy data email change

--- components/Gdpr/GdprData.ts
+++ components/Gdpr/GdprData.ts
@@ -1,50 +1,50 @@
 export const staticData = {
-  banner: {
-    title: `Folosim cookie-uri`,
-    desc: `Folosim cookie-uri pentru a vă îmbunătăți experiența de navigare și pentru a personaliza conținutul pe care îl prezentăm. Meniul de preferinţe vă permite să controlați tipurile de cookie-uri pe care le acceptați.`,
-    btnPrivacy: `Politica de confidenţialitate`,
-    btnPrefs: `Preferinţe`,
-    btnRefuse: `Refuz toate`,
-    btnAccetp: `Accept toate`,
-  },
-  preferences: {
-    title: `Preferinţe`,
-    seeMore: `Vezi mai multe pe`,
-    desc: `Folosim cookie-uri pentru a vă îmbunătăți experiența de navigare și pentru a personaliza conținutul pe care îl prezentăm. Meniul de preferinţe vă permite să controlați tipurile de cookie-uri pe care le acceptați.`,
-    essentialTitle: `Esenţiale`,
-    essentialDesc: `Pentru funcţionarea site-ului. Ne trebuie măcar 1 cookie pentru a stoca alegerea dvs. (numele acestuia este "consentChoice")`,
-    prefsTitle: `Preferinţă`,
-    prefDesc: `Pentru a putea vizualiza anumite tipuri de conţinut (exemplu: postări de pe Facebook sau video-uri de pe YouTube ce utilizează cookie-uri)`,
-    analyticsTitle: `Analitice`,
-    analyticsDesc: `Pentru a măsura traficul şi performanţa website-ului`,
-    adsTitle: `Publicitate`,
-    adsDesc: `Pentru a putea personaliza anunţurile publicitare. (În acest moment nu folosim anunţuri)`,
-    langTitle: `Limba engleză`,
-    langDesc: `Pentru a putea personaliza limba`,
-    save: "Salvează",
-  },
-  privacy: {
-    body: [
-      `Introducere`,
-      `Auroralove Srl ("noi", "noi" sau "nostru") vă respectă confidențialitatea și se angajează să vă protejeze datele personale. Această politică de confidențialitate vă va informa despre modul în care colectăm, utilizăm și vă protejăm datele personale atunci când vizitați site-ul nostru web și cumpărați produsele noastre.`,
-      `Operator de date
-Operatorul de date al datelor dumneavoastră cu caracter personal este Auroralove Srl, J22/514/2023, business id: 47638588, cu email auroralove.website@gmail.com.
+    banner: {
+        title: `Folosim cookie-uri`,
+        desc: `Folosim cookie-uri pentru a vă îmbunătăți experiența de navigare și pentru a personaliza conținutul pe care îl prezentăm. Meniul de preferinţe vă permite să controlați tipurile de cookie-uri pe care le acceptați.`,
+        btnPrivacy: `Politica de confidenţialitate`,
+        btnPrefs: `Preferinţe`,
+        btnRefuse: `Refuz toate`,
+        btnAccetp: `Accept toate`,
+    },
+    preferences: {
+        title: `Preferinţe`,
+        seeMore: `Vezi mai multe pe`,
+        desc: `Folosim cookie-uri pentru a vă îmbunătăți experiența de navigare și pentru a personaliza conținutul pe care îl prezentăm. Meniul de preferinţe vă permite să controlați tipurile de cookie-uri pe care le acceptați.`,
+        essentialTitle: `Esenţiale`,
+        essentialDesc: `Pentru funcţionarea site-ului. Ne trebuie măcar 1 cookie pentru a stoca alegerea dvs. (numele acestuia este "consentChoice")`,
+        prefsTitle: `Preferinţă`,
+        prefDesc: `Pentru a putea vizualiza anumite tipuri de conţinut (exemplu: postări de pe Facebook sau video-uri de pe YouTube ce utilizează cookie-uri)`,
+        analyticsTitle: `Analitice`,
+        analyticsDesc: `Pentru a măsura traficul şi performanţa website-ului`,
+        adsTitle: `Publicitate`,
+        adsDesc: `Pentru a putea personaliza anunţurile publicitare. (În acest moment nu folosim anunţuri)`,
+        langTitle: `Limba engleză`,
+        langDesc: `Pentru a putea personaliza limba`,
+        save: "Salvează",
+    },
+    privacy: {
+        body: [
+            `Introducere`,
+            `Auroralove Srl ("noi", "noi" sau "nostru") vă respectă confidențialitatea și se angajează să vă protejeze datele personale. Această politică de confidențialitate vă va informa despre modul în care colectăm, utilizăm și vă protejăm datele personale atunci când vizitați site-ul nostru web și cumpărați produsele noastre.`,
+            `Operator de date
+Operatorul de date al datelor dumneavoastră cu caracter personal este Auroralove Srl, J22/514/2023, business id: 47638588, cu email magda.dimitrescu.website@gmail.com.
 `,
-      `Datele personale pe care le colectăm`,
-      `
+            `Datele personale pe care le colectăm`,
+            `
 Colectăm și procesăm următoarele tipuri de date cu caracter personal:
 Informațiile pe care ni le furnizați atunci când efectuați o achiziție, inclusiv numele, e-mailul, adresa, orașul, codul poștal și adresa IP.
 Informații pe care le colectăm automat atunci când vizitați site-ul nostru web, inclusiv adresa IP a dispozitivului dvs., tipul și versiunea browserului, tipul și versiunea sistemului de operare și paginile pe care le vizitați pe site-ul nostru.
 Informații pe care le primim de la servicii terțe, inclusiv Google Analytics, Stripe, Vercel
 `,
-      `Temei juridic pentru prelucrare`,
-      `
+            `Temei juridic pentru prelucrare`,
+            `
 Prelucrăm datele dumneavoastră cu caracter personal pe baza următoarelor baze legale:
 Executarea unui contract: Prelucrăm datele dumneavoastră cu caracter personal pentru a ne îndeplini obligațiile față de dumneavoastră atunci când cumpărați produsele noastre.
 Interese legitime: Prelucrăm datele dumneavoastră cu caracter personal pentru a îmbunătăți site-ul și produsele noastre, pentru a preveni frauda și pentru a ne proteja drepturile legale.
 `,
-      `Cum folosim datele dumneavoastră cu caracter personal`,
-      `
+            `Cum folosim datele dumneavoastră cu caracter personal`,
+            `
 Utilizăm datele dumneavoastră cu caracter personal în următoarele scopuri:
 Pentru a vă procesa comenzile și a vă oferi produsele și serviciile noastre.
 Pentru a comunica cu dvs. despre comenzile, plățile și alte întrebări legate de serviciul clienți.
@@ -52,23 +52,23 @@ Pentru a ne îmbunătăți site-ul web și produsele pe baza feedback-ului și u
 Pentru a analiza traficul site-ului și comportamentul utilizatorilor folosind Google Analytics.
 Pentru a vă trimite comunicări de marketing, dacă v-ați înscris să le primiți.
 `,
-      `Cui împărtășim datele dumneavoastră cu caracter personal`,
-      `
+            `Cui împărtășim datele dumneavoastră cu caracter personal`,
+            `
 Putem partaja datele dumneavoastră cu caracter personal cu următoarele categorii de destinatari:
 Furnizorii de servicii care ne ajută să operam site-ul nostru web și să procesăm comenzile dvs., inclusiv Stripe, Nodemailer și Pocketbase SqlLite.
 Furnizori terți de analize, inclusiv Google Analytics.
 Autorități juridice și de reglementare, dacă este cerut de lege.
 `,
-      `Transferuri de date`,
-      `
+            `Transferuri de date`,
+            `
 Putem transfera datele dumneavoastră cu caracter personal către destinatari din afara Spațiului Economic European (SEE), dacă este necesar pentru scopurile descrise în această politică de confidențialitate. Ne asigurăm că astfel de transferuri sunt efectuate în conformitate cu GDPR prin utilizarea garanțiilor adecvate, cum ar fi clauze contractuale standard.
 `,
-      `Reținerea datelor`,
-      `
+            `Reținerea datelor`,
+            `
 Reținem datele dumneavoastră cu caracter personal atât timp cât este necesar pentru îndeplinirea scopurilor pentru care au fost colectate, cu excepția cazului în care legea impune o perioadă mai lungă de păstrare.
 `,
-      `Drepturile tale`,
-      `
+            `Drepturile tale`,
+            `
 Aveți următoarele drepturi cu privire la datele dumneavoastră personale:
 Dreptul de acces: Puteți solicita accesul la datele personale pe care le deținem despre dumneavoastră.
 Dreptul la rectificare: Puteți solicita să corectăm orice date cu caracter personal inexacte sau incomplete pe care le deținem despre dumneavoastră.
@@ -80,74 +80,74 @@ Dreptul de a retrage consimțământul: Vă puteți retrage oricând consimțăm
 Dacă doriți să vă exercitați oricare dintre aceste drepturi, vă rugăm să ne contactați folosind datele de contact furnizate la sfârșitul acestei politici de confidențialitate.
 
 `,
-      `Cookies`,
-      `
+            `Cookies`,
+            `
 Folosim cookie-uri și tehnologii similare pentru a vă îmbunătăți experiența pe site-ul nostru web, pentru a analiza traficul pe site și pentru a personaliza comunicările de marketing. Când vizitați site-ul nostru web, va fi afișat un banner de notificare privind cookie-urile și puteți alege să acceptați sau să respingeți cookie-urile neesențiale.
 `,
-      `Măsuri de securitate`,
-      `
+            `Măsuri de securitate`,
+            `
 Luăm măsuri tehnice și organizatorice adecvate pentru a vă proteja datele personale împotriva prelucrării neautorizate sau ilegale, pierderii accidentale, distrugerii sau deteriorarii. Cu toate acestea, nicio metodă de transmitere prin Internet sau stocare electronică nu este 100% sigură și nu putem garanta securitatea absolută a datelor dumneavoastră personale.
 `,
-      `Modificări ale acestei politici de confidențialitate`,
-      `
+            `Modificări ale acestei politici de confidențialitate`,
+            `
 Este posibil să actualizăm această politică de confidențialitate din când în când pentru a reflecta schimbările în practicile noastre sau cerințele legale. Vă vom anunța cu privire la orice modificări materiale prin publicarea politicii de confidențialitate actualizate pe site-ul nostru web sau prin e-mail.
 `,
-      `Contactați-ne`,
-      `
-Dacă aveți întrebări sau nelămuriri cu privire la această politică de confidențialitate sau practicile noastre cu privire la datele dumneavoastră cu caracter personal, vă rugăm să ne contactați la auroralove.website@gmail.com.
+            `Contactați-ne`,
+            `
+Dacă aveți întrebări sau nelămuriri cu privire la această politică de confidențialitate sau practicile noastre cu privire la datele dumneavoastră cu caracter personal, vă rugăm să ne contactați la magda.dimitrescu.website@gmail.com.
 `,
-    ],
-  },
+        ],
+    },
 };
 
 export const staticDataEn = {
-  banner: {
-    title: `We use cookies`,
-    desc: `We use cookies to enhance your browsing experience and to personalize the content we present to you. The preferences menu allows you to control the types of cookies you accept.`,
-    btnPrivacy: `Privacy Policy`,
-    btnPrefs: `Preferences`,
-    btnRefuse: `Refuse all`,
-    btnAccetp: `Accept all`,
-  },
-  preferences: {
-    title: `Preferences`,
-    seeMore: `See more on`,
-    desc: `We use cookies to enhance your browsing experience and to personalize the content we present to you. The preferences menu allows you to control the types of cookies you accept.`,
-    essentialTitle: `Essential`,
-    essentialDesc: `For site functionality. We need at least 1 cookie to store your choice. (Its name is "consentChoice")`,
-    prefsTitle: `Preference`,
-    prefDesc: `To view certain types of content (e.g. Facebook posts or YouTube videos that use cookies)`,
-    analyticsTitle: `Analytics`,
-    analyticsDesc: `To measure website traffic and performance`,
-    adsTitle: `Advertising`,
-    adsDesc: `To personalize advertising. (We are currently not using ads)`,
-    langTitle: `English language`,
-    langDesc: `To personalize the language`,
-    save: "Save",
-  },
+    banner: {
+        title: `We use cookies`,
+        desc: `We use cookies to enhance your browsing experience and to personalize the content we present to you. The preferences menu allows you to control the types of cookies you accept.`,
+        btnPrivacy: `Privacy Policy`,
+        btnPrefs: `Preferences`,
+        btnRefuse: `Refuse all`,
+        btnAccetp: `Accept all`,
+    },
+    preferences: {
+        title: `Preferences`,
+        seeMore: `See more on`,
+        desc: `We use cookies to enhance your browsing experience and to personalize the content we present to you. The preferences menu allows you to control the types of cookies you accept.`,
+        essentialTitle: `Essential`,
+        essentialDesc: `For site functionality. We need at least 1 cookie to store your choice. (Its name is "consentChoice")`,
+        prefsTitle: `Preference`,
+        prefDesc: `To view certain types of content (e.g. Facebook posts or YouTube videos that use cookies)`,
+        analyticsTitle: `Analytics`,
+        analyticsDesc: `To measure website traffic and performance`,
+        adsTitle: `Advertising`,
+        adsDesc: `To personalize advertising. (We are currently not using ads)`,
+        langTitle: `English language`,
+        langDesc: `To personalize the language`,
+        save: "Save",
+    },
 
-  privacy: {
-    body: [
-      `Introduction`,
-      `Auroralove Srl ("we", "us", or "our") respects your privacy and is committed to protecting your personal data. This privacy policy will inform you about how we collect, use, and protect your personal data when you visit our website and purchase our products.`,
-      `Data Controller
-The data controller of your personal data is Auroralove Srl, J22/514/2023, business id: 47638588, with the email auroralove.website@gmail.com.
+    privacy: {
+        body: [
+            `Introduction`,
+            `Auroralove Srl ("we", "us", or "our") respects your privacy and is committed to protecting your personal data. This privacy policy will inform you about how we collect, use, and protect your personal data when you visit our website and purchase our products.`,
+            `Data Controller
+The data controller of your personal data is Auroralove Srl, J22/514/2023, business id: 47638588, with the email magda.dimitrescu.website@gmail.com.
 `,
-      `Personal Data We Collect`,
-      `
+            `Personal Data We Collect`,
+            `
 We collect and process the following types of personal data:
 Information you provide to us when you make a purchase, including your name, email, address, city, zip code, and IP address.
 Information we collect automatically when you visit our website, including your device's IP address, browser type and version, operating system type and version, and pages you visit on our website.
 Information we receive from third-party services, including Google Analytics, Stripe, Vercel
 `,
-      `Legal Basis for Processing`,
-      `
+            `Legal Basis for Processing`,
+            `
 We process your personal data based on the following legal bases:
 Performance of a contract: We process your personal data to fulfill our obligations to you when you purchase our products.
 Legitimate interests: We process your personal data to improve our website and products, prevent fraud, and protect our legal rights.
 `,
-      `How We Use Your Personal Data`,
-      `
+            `How We Use Your Personal Data`,
+            `
 We use your personal data for the following purposes:
 To process your orders and provide you with our products and services.
 To communicate with you about your orders, payments, and other customer service inquiries.
@@ -155,23 +155,23 @@ To improve our website and products based on your feedback and usage.
 To analyze website traffic and user behavior using Google Analytics.
 To send you marketing communications, if you have opted-in to receive them.
 `,
-      `Who We Share Your Personal Data With`,
-      `
+            `Who We Share Your Personal Data With`,
+            `
 We may share your personal data with the following categories of recipients:
 Service providers who help us operate our website and process your orders, including Stripe, Nodemailer, and Pocketbase SqlLite.
 Third-party analytics providers, including Google Analytics.
 Legal and regulatory authorities, if required by law.
 `,
-      `Data Transfers`,
-      `
+            `Data Transfers`,
+            `
 We may transfer your personal data to recipients outside the European Economic Area (EEA) if necessary for the purposes described in this privacy policy. We ensure that such transfers are made in compliance with the GDPR by using appropriate safeguards, such as standard contractual clauses.
 `,
-      `Data Retention`,
-      `
+            `Data Retention`,
+            `
 We retain your personal data for as long as necessary to fulfill the purposes for which it was collected, unless a longer retention period is required by law.
 `,
-      `Your Rights`,
-      `
+            `Your Rights`,
+            `
 You have the following rights regarding your personal data:
 Right to access: You can request access to the personal data we hold about you.
 Right to rectification: You can request that we correct any inaccurate or incomplete personal data we hold about you.
@@ -183,22 +183,22 @@ Right to withdraw consent: You can withdraw your consent to the processing of yo
 If you wish to exercise any of these rights, please contact us using the contact details provided at the end of this privacy policy.
 
 `,
-      `Cookies`,
-      `
+            `Cookies`,
+            `
 We use cookies and similar technologies to improve your experience on our website, analyze website traffic, and personalize marketing communications. When you visit our website, a cookie notice banner will be displayed, and you can choose to accept or reject non-essential cookies.
 `,
-      `Security Measures`,
-      `
+            `Security Measures`,
+            `
 We take appropriate technical and organizational measures to protect your personal data against unauthorized or unlawful processing, accidental loss, destruction, or damage. However, no method of transmission over the Internet or electronic storage is 100% secure, and we cannot guarantee the absolute security of your personal data.
 `,
-      `Changes to this Privacy Policy`,
-      `
+            `Changes to this Privacy Policy`,
+            `
 We may update this privacy policy from time to time to reflect changes in our practices or legal requirements. We will notify you of any material changes by posting the updated privacy policy on our website or by email.
 `,
-      `Contact Us`,
-      `
-If you have any questions or concerns about this privacy policy or our practices regarding your personal data, please contact us at auroralove.website@gmail.com.
+            `Contact Us`,
+            `
+If you have any questions or concerns about this privacy policy or our practices regarding your personal data, please contact us at magda.dimitrescu.website@gmail.com.
 `,
-    ],
-  },
+        ],
+    },
 };
